7b4e8b8e7bdd7faf51029ff83efe3d6afcd0f79a
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-03-24 12:34:12 +0100

    - remove votes association

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -3,7 +3,6 @@ class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :ingredients, :preparation_description, :created_at, :votes
   # model association
   belongs_to :user, key: :owner
-  has_many :votes
 
   def votes
     object.get_votes
