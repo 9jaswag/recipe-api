95d951d3f2c4ef95c648b46f4e1e851ad274ed59
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-03-24 12:32:09 +0100

    - add vote attribute in serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,6 @@
 class RecipeSerializer < ActiveModel::Serializer
   # attributes to be serialized
-  attributes :id, :name, :image, :ingredients, :preparation_description, :created_at
+  attributes :id, :name, :image, :ingredients, :preparation_description, :created_at, :votes
   # model association
   belongs_to :user, key: :owner
   has_many :votes
